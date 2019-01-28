8dfcec1ec0b8f832e2e4b9b8bff5a0764b6b2eac
Paul Hallett <paul.hallett@babylonhealth.com>
2019-01-28 15:00:45 +0000

    add relay example

--- flask/schema.py
+++ flask/schema.py
@@ -2,8 +2,14 @@ import graphene
 
 
 class Book(graphene.ObjectType):
-
+    class Meta:
+        interfaces = (graphene.relay.Node,)
     name = graphene.String()
+
+
+class BookConnection(graphene.relay.Connection):
+    class Meta:
+        node = Book
 
 
 class CreateBook(graphene.Mutation):
@@ -21,10 +27,17 @@ class CreateBook(graphene.Mutation):
 
 class Query(graphene.ObjectType):
     book = graphene.Field(Book, name=graphene.String())
+    books = graphene.relay.connection.IterableConnectionField(
+        BookConnection, name_contains=graphene.String(),
+    )
 
     def resolve_book(self, info, name):
         # Perform any look-up logic you want here.
         return Book(name=name)
+
+    def resolve_books(self, info, name_contains):
+        # Filter a collection here and return an array of ObjectTypes
+        return [Book(name=name_contains)]
 
 class Mutations(graphene.ObjectType):
     create_book = CreateBook.Field()
